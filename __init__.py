3f878d218aeef0498a3012ca1cbc42950adce658
amaan2398 <amaan.nassar123@gmail.com>
2020-06-07 16:47:35 +0530

    Final changes for  Instructions for Deployment

--- __init__.py
+++ __init__.py
@@ -15,28 +15,15 @@ import requests
 from flask import Flask, request,render_template,jsonify
 
 app = Flask(__name__,template_folder='template')
-model=pickle.load(open('D:\\Final project\\Program\\MLModel\\finalized_Decision_Tree_80+20_split.sav','rb'))
+model = pickle.load(open('.\\MLModel\\NuralNetworkModel.sav','rb'))
+model2 = pickle.load(open('.\\MLModel\\finalized_Decision_Tree_80+20_split.sav','rb'))
 
-@app.route("/discount",methods=['POST'])
-def test():
-    print (request.is_json)
-    woocomm=request.get_json()
-    print(woocomm)
-    
-    x =  '{"response":'+str(random.randrange(0,3,1))+'}'
-    y = json.loads(x)
-    return y
-    
 @app.route("/discount_model",methods=['POST'])
 def dis_prediction_model():
-    pidDic={31:100,32:101,33:102,34:103,35:104,36:105,37:106,38:107,39:108,40:109,41:110,42:111}
-    #get input
+    pidDic={31 : 101,32 : 107,33 : 112,34 : 108,35 : 106,36 : 102,37 : 109,38 : 103,39 : 111,40 : 104,41 : 110,42 : 105}
     woocomm=request.get_json()
-    user = 'ZDEzZWRkNTllZDk0NjQxN2RjYWZhNmIy'
-    #pythonapp = 'G4kN hBNh r35J luXk aXyd n6Lm'
+    user = '' #add security code.
     url = 'http://localhost/wordpress/wp-json/ld/v1'
-    #token = base64.standard_b64encode(user + ':' + pythonapp)
-    #headers = {'SecurityKey': 'Basic ' + token}
     headers = {'SecurityKey':user}
     r = requests.post(url + '/posts/'+str(woocomm['id'])+'?pid='+str(woocomm['product_c']), headers=headers)
     ip=r.json()
@@ -58,9 +45,11 @@ def dis_prediction_model():
         Todydf.date=Todydf.sort_values(by='date')
         r=(Todydf.date.sub(df.iloc[Freq-1].Date)/np.timedelta64(1,'D')).iloc[0]
         tdf=pd.DataFrame({"Recency":[r],"Frequency":[Freq],"Monetary":[Pamt],"CCategory_int":[pidDic[int(woocomm['product_c'])]]})
-        result=model.predict(tdf)
+        result1 = model.predict(tdf)
+        result = model2.predict(tdf)
         r = requests.post(url + '/discount/'+str(woocomm['product_c']), headers=headers)
         rlt=r.json()
+        print(result1,"Nural Network")
         print(result,"RESULT")
         resp=0
         if result[0]==3:
@@ -68,13 +57,13 @@ def dis_prediction_model():
             resp=0
         elif result[0]==1 and int(ip[0]['lastOfferCode'])%10<3:
             code=int(ip[0]['lastOfferCode'])+1
-            resp=rlt[0][str(result[0])]
+            resp=rlt[0]["0"]
         elif result[0]==2 and (int(ip[0]['lastOfferCode'])//10)%10<2:
             code=int(ip[0]['lastOfferCode'])+10
-            resp=rlt[0][str(result[0])]
+            resp=rlt[0]["1"]
         elif result[0]==0 and (int(ip[0]['lastOfferCode'])//100)%10<1:
             code=int(ip[0]['lastOfferCode'])+100
-            resp=rlt[0][str(result[0])]
+            resp=rlt[0]["2"]
         elif int(ip[0]['lastOfferCode'])>100:
             code=0
             resp=0
@@ -89,6 +78,7 @@ def dis_prediction_model():
     
 
 
-# running web app in local machine
 if __name__ == '__main__':
-    app.run(host='192.168.1.107', port='8099')#in plasc of ... use your ip or domain name to host your server
+    app.run()
+        
+    
